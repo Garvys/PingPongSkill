944f1bfa702d2df5478c47746ba3c5742b0006b6
Garvys <alexandre.caulier@orange.fr>
2017-09-22 12:14:55 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
 
 setup(
-    name='myskill',
+    name='ping_pong_skill',
     version='0.0.1',
     description='My skill for Snips',
-    url='https://github.com/snipsco/snips-skill-skeleton',
+    url='https://github.com/Garvys/PingPongSkill',
     download_url='',
     license='MIT',
     install_requires=['requests==2.6.0'],
     test_suite="tests",
     keywords=['snips'],
-    packages=['myskill'],
+    packages=['ping_pong_skill'],
     package_data={'myskill': ['Snipsspec']},
     include_package_data=True,
 )
