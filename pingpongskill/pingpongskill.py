24f9798102abcd714fe4a17f2cff6084d6b086fc
Garvys <alexandre.caulier@orange.fr>
2017-09-22 15:40:49 +0200

    Print

--- pingpongskill/pingpongskill.py
+++ pingpongskill/pingpongskill.py
@@ -7,7 +7,7 @@ import json
 
 
 
-class PingPongSkill:
+class PingPongSkill(object):
     """ Skeleton Snips skill. """
 
     def __init__(self):
@@ -21,5 +21,6 @@ class PingPongSkill:
 
     def handle_terminate_game(self, intent):
         print "*** {}".format(intent)
+        print "* {}".format(intent.__dict__)
         with open('/home/pi/intent.json', 'w') as f:
-            json.dump(intent, f)
+            json.dump(intent.__dict__, f)
